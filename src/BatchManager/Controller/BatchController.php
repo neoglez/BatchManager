eefcc7f8c3f8a2319c3e39f96b226dea5c68346d
neoglez <neoglez@gmail.com>
2014-06-27 16:11:11 +0200

    Update batch controller
    [skip ci]

--- src/BatchManager/Controller/BatchController.php
+++ src/BatchManager/Controller/BatchController.php
@@ -27,6 +27,8 @@ class BatchController extends AbstractActionController
             'action' => $pAction,
             $bidKey => $batch->getBid()
         );
+        $queryParams = $this->params()->fromQuery();
+        $queryParams = !empty($queryParams) ? array('query' => $queryParams): null;
         
         list($fRoute, $fAction, $bidKey) = $moduleOptions->getFinishedRouteConfigArray();
         $fparams = array(
@@ -39,8 +41,8 @@ class BatchController extends AbstractActionController
                 'percentage' => $batchManager->getPercentage(),
                 'message' => $batchManager->getCurrentMessage(),
                 'metaRefreshSeconds' => $moduleOptions->getMetaRefreshSecons(),
-                'refreshUrl' => $this->url()->fromRoute($pRoute, $params),
-                'finishedUrl' => $this->url()->fromRoute($fRoute, $fparams),
+                'refreshUrl' => $this->url()->fromRoute($pRoute, $params, $queryParams),
+                'finishedUrl' => $this->url()->fromRoute($fRoute, $fparams, $queryParams),
                 'batchId' => $batch->getBid()
             );
             
@@ -63,6 +65,9 @@ class BatchController extends AbstractActionController
         // If we found batch key in request object we set them in the batch
         $batchId = $this->params($moduleOptions->getIdKeyInRequest());
         
+        $queryParams = $this->params()->fromQuery();
+        $queryParams = !empty($queryParams) ? array('query' => $queryParams): null;
+        
         /*@var $batch \BatchManager\Entity\Batch */
         $batch = $batchManager->getBatch();
         
@@ -80,28 +85,66 @@ class BatchController extends AbstractActionController
             return $result;
         }
         
+        $pRoute = $moduleOptions->getProcessRoute();
         
         list($fRoute, $fAction, $bidKey) = $moduleOptions->getFinishedRouteConfigArray();
-        $params = array(
+        $fParams = array(
             'action' => $fAction,
             $bidKey => $batch->getBid()
         );
         
+        $queryParams = $this->params()->fromQuery();
+        $queryParams = !empty($queryParams) ? array('query' => $queryParams): array();
         
         // the view script will take care of setting the meta tags
         $model = array(
             'percentage' => $batchManager->getPercentage(),
             'message' => $batchManager->getCurrentMessage(),
             'metaRefreshSeconds' => $moduleOptions->getMetaRefreshSecons(),
-            'refreshUrl' => $this->url()->fromRoute($fRoute, $params),
+            'refreshUrl' => $this->url()->fromRoute($pRoute, array(), $queryParams, true),
             'batchId' => $batch->getBid(),
             'finished' => ($batchManager->getPercentage() >= 100)
         );
+        if ($batchManager->getPercentage() >= 100) {
+            $model['refreshUrl'] = $this->url()->fromRoute($fRoute, $fParams, $queryParams);
+        }
         return new ViewModel($model);
     }
     
     public function finishedAction()
     {
+        // Get the Batch manager and process the batch
+        /*@var $batchManager \BatchManager\Service\BatchManager */
+        $batchManager = $this->getServiceLocator()->get('batch_manager');
         
+        if (!$batchManager->isError()) {
+            /*@var $moduleOptions \BatchManager\Option\ModuleOptions */
+            $moduleOptions = $this->getServiceLocator()->get('batch_manager_module_options');
+            
+            // If we found batch key in request object we set them in the batch
+            $batchId = $this->params($moduleOptions->getIdKeyInRequest());
+            
+            /*@var $batch \BatchManager\Entity\Batch */
+            $batch = $batchManager->getBatch();
+            
+            if ($batchId) {
+                $batch->setBid($batchId);
+                $batchManager->setBatch($batch);
+            }
+            
+            $result = $batchManager->finishBatch();
+            // If someone return a response shortcut right away.
+            // We enforce a HTTP response
+            if ($result instanceof HTTPResponse) {
+                return $result;
+            }
+        }
+        
+        $model = array(
+            'message' => $batchManager->getCurrentMessage(),
+            'isError' => $batchManager->isError(),
+            'finished' => ($batchManager->getPercentage() >= 100)
+        );
+        return new ViewModel($model);
     }
 }
